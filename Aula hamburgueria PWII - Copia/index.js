61019ef73e4bb0773dc9717181682ca3209a3b5b
Felipe Oliveira <118781386+Felipnasc@users.noreply.github.com>
2023-03-20 20:28:48 -0300

    AULA 1

---
+++ Aula hamburgueria PWII - Copia/index.js
@@ -0,0 +1,8 @@
+/*IMPORT DO PACOTE EXPRESS*/
+const express = require('express');
+
+/*INSTANCIA EXECUTAVEL DO EXPRESS*/
+const app = express();
+
+/*HABILITA A APLICAÇÃO A MANIPULAR JSON*/
+app.use(express.json());
