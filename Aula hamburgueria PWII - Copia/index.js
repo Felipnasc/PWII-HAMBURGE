27eb00b632065dda5fec122958241bb263431998
Felipe Oliveira <118781386+Felipnasc@users.noreply.github.com>
2023-03-20 21:50:17 -0300

    Add files via upload

--- Aula hamburgueria PWII - Copia/index.js
+++ Aula hamburgueria PWII - Copia/index.js
@@ -6,3 +6,7 @@ const app = express();
 
 /*HABILITA A APLICAÇÃO A MANIPULAR JSON*/
 app.use(express.json());
+
+app.listen(3000, ()=>{ 
+    console.log('SERVIDOR RODANDO EM - http://localhost:3000'); 
+});
